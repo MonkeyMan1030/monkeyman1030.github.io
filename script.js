e445656083c085e3fcf27bc02e56639c29305d0b
MonkeyMan1030 <132142256+MonkeyMan1030@users.noreply.github.com>
2023-04-29 09:05:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const yesButton = document.getElementById("yesButton");
 const noButton = document.getElementById("noButton");
 
 yesButton.addEventListener("click", function() {
-  window.location.href = "https://www.youtube.com/";
+  window.location.href = "https://www.youtube.com/watch?v=nLYGO8GwM4g";
 });
 
 noButton.addEventListener("click", function() {
